bf2ca2aaa6ac5a4d024b30904256f2b8850a1164
dominiquebuford <97184076+dominiquebuford@users.noreply.github.com>
2024-07-23 15:46:07 -0500

    format and clean up code

--- backend/app.py
+++ backend/app.py
@@ -9,10 +9,19 @@ import json
 
 app = Flask(__name__)
 cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+app.config["CORS_HEADERS"] = "Content-Type"
 
 
 def image_to_base64(file):
+    """
+    Converts an image file to base64 encoding.
+
+    Args:
+    - file: File object representing the image file.
+
+    Returns:
+    - str: Base64 encoded string of the image data, or None if error.
+    """
     try:
         image_data = file.read()
         base64_representation = base64.b64encode(image_data).decode("utf-8")
@@ -22,13 +31,26 @@ def image_to_base64(file):
         return None
 
 
-@app.route('/findSimilar', methods=['POST'])
+@app.route("/findSimilar", methods=["POST"])
 @cross_origin()
 def find_similar_images():
-    embeddings = json.loads(request.form['embeddings'])
-    ids = json.loads(request.form['ids'])
-    searchTerm = request.form['searchTerm']
-    model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+    """
+    Endpoint to find similar images based on embeddings and search term.
+
+    Expects POST request with form data:
+    - embeddings: JSON array of embeddings.
+    - ids: JSON array of IDs corresponding to embeddings.
+    - searchTerm: Search term to compare against embeddings.
+
+    Uses SentenceTransformer model for embedding similarity calculation.
+
+    Returns:
+    - JSON: List of IDs of images similar to the search term.
+    """
+    embeddings = json.loads(request.form["embeddings"])
+    ids = json.loads(request.form["ids"])
+    searchTerm = request.form["searchTerm"]
+    model = SentenceTransformer("paraphrase-MiniLM-L6-v2")
     searchTerm_embedding = model.encode(searchTerm, convert_to_numpy=True)
 
     # Calculate cosine similarity for each embedding
@@ -45,15 +67,23 @@ def find_similar_images():
     return jsonify(results)
 
 
-@app.route('/upload', methods=['POST'])
+@app.route("/upload", methods=["POST"])
 @cross_origin()
 def upload_file():
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'}), 400
-    file = request.files['file']
-    filename = request.form.get('name', '')
-    if filename == '':
-        return jsonify({'error': 'No selected file'}), 400
+    """
+    Endpoint to handle file uploads and image description retrieval.
+
+    Expects POST request with a file attached and 'name' field.
+
+    Returns:
+    - JSON: Response containing AI-generated image description and embedding.
+    """
+    if "file" not in request.files:
+        return jsonify({"error": "No file part"}), 400
+    file = request.files["file"]
+    filename = request.form.get("name", "")
+    if filename == "":
+        return jsonify({"error": "No selected file"}), 400
     if file:
         base64_image = image_to_base64(file)
 
@@ -61,27 +91,27 @@ def upload_file():
             payload = {
                 "stream": False,
                 "image_data": [{"id": 10, "data": base64_image}],
-                "prompt": "You are an AI assistant that describes images " +
-                "in a descriptive phrase " +
-                "between 15 and 30 words. Describe" +
-                "everything you see. " +
-                "\nUSER: What does the [img-10] contain?\n" +
-                "ASSISTANT:"
-                }
+                "prompt": "You are an AI assistant that describes images "
+                + "in a descriptive phrase "
+                + "between 15 and 30 words. Describe"
+                + "everything you see. "
+                + "\nUSER: What does the [img-10] contain?\n"
+                + "ASSISTANT:",
+            }
             headers = {"Content-Type": "application/json"}
-            response = requests.post('http://host.docker.internal:8080' +
-                                     '/completion',
-                                     headers=headers, json=payload)
-            print(response)
+            response = requests.post(
+                "http://host.docker.internal:8080" + "/completion",
+                headers=headers,
+                json=payload,
+            )
             r = response.json()
 
-            model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+            model = SentenceTransformer("paraphrase-MiniLM-L6-v2")
             embedding = model.encode(r["content"], convert_to_numpy=True)
 
-            tensor_json_serializable = embedding.tolist()
+            tensor_json_serial = embedding.tolist()
 
-            return {'answer': r["content"],
-                    'embedding': tensor_json_serializable}
+            return {"answer": r["content"], "embedding": tensor_json_serial}
 
 
 if __name__ == "__main__":
--- backend/test_app.py
+++ backend/test_app.py
@@ -15,7 +15,7 @@ def client():
 
 
 def test_image_to_base64_success():
-    with open("backend/tests/test_image.jpg", "rb") as file:
+    with open("backend/tests/img1.jpg", "rb") as file:
         base64_string = image_to_base64(file)
         assert base64_string is not None
 
@@ -25,37 +25,38 @@ def test_find_similar_images(client):
     mock_ids = ["id1", "id2", "id3"]
     mock_search_term = "example search term"
 
-    with patch('app.SentenceTransformer') as mock_model:
+    with patch("app.SentenceTransformer") as mock_model:
         mock_instance = mock_model.return_value
         mock_instance.encode.return_value = np.array([0.1, 0.2, 0.3])
 
-        response = client.post('/findSimilar', data={
-            'embeddings': json.dumps(mock_embeddings),
-            'ids': json.dumps(mock_ids),
-            'searchTerm': mock_search_term
-        })
+        response = client.post(
+            "/findSimilar",
+            data={
+                "embeddings": json.dumps(mock_embeddings),
+                "ids": json.dumps(mock_ids),
+                "searchTerm": mock_search_term,
+            },
+        )
 
         assert response.status_code == 200
         assert json.loads(response.data) == ["id1", "id2"]
 
 
 def test_upload_file_no_file(client):
-    response = client.post('/upload', data={})
+    response = client.post("/upload", data={})
     assert response.status_code == 400
-    assert response.get_json() == {'error': 'No file part'}
+    assert response.get_json() == {"error": "No file part"}
 
 
 def test_upload_file_no_filename(client):
-    data = {
-        'file': (open('backend/tests/test_image.jpg', 'rb'), 'test_image.jpg')
-    }
-    response = client.post('/upload', data=data)
+    data = {"file": (open("backend/tests/img1.jpg", "rb"), "img1.jpg")}
+    response = client.post("/upload", data=data)
     assert response.status_code == 400
-    assert response.get_json() == {'error': 'No selected file'}
+    assert response.get_json() == {"error": "No selected file"}
 
 
-@patch('app.requests.post')
-@patch('app.SentenceTransformer')
+@patch("app.requests.post")
+@patch("app.SentenceTransformer")
 def test_upload_file_success(mock_model, mock_requests, client):
     mock_model_instance = mock_model.return_value
     mock_model_instance.encode.return_value = np.array([0.1, 0.2, 0.3])
@@ -65,15 +66,14 @@ def test_upload_file_success(mock_model, mock_requests, client):
     mock_requests.return_value = mock_response
 
     data = {
-        'file': (open('backend/tests/test_image.jpg', 'rb'), 'test_image.jpg'),
-        'name': 'test_image'
+        "file": (open("backend/tests/img1.jpg", "rb"), "img1.jpg"),
+        "name": "test_image",
     }
-    response = client.post('/upload', data=data,
-                           content_type='multipart/form-data')
+    response = client.post("/upload", data=data)
 
     assert response.status_code == 200
     response_data = response.get_json()
-    assert 'answer' in response_data
-    assert 'embedding' in response_data
-    assert response_data['answer'] == 'A descriptive phrase'
-    assert response_data['embedding'] == [0.1, 0.2, 0.3]
+    assert "answer" in response_data
+    assert "embedding" in response_data
+    assert response_data["answer"] == "A descriptive phrase"
+    assert response_data["embedding"] == [0.1, 0.2, 0.3]
